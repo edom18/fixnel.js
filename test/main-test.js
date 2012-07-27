e222a6d530f6c90a1c0be9ecf344402702ac29a4
edom18 <edo.m18@gmail.com>
2012-07-27 23:06:53 +0900

    modified a few test modules.

--- test/main-test.js
+++ test/main-test.js
@@ -2,32 +2,9 @@
 
     'use strict';
 
-    var dot = document.querySelector('#inner');
-    var p = new Fixnel(dot);
+    var f1 = document.querySelector('#f1');
+    var f2 = document.querySelector('#f2');
+    var p = new Fixnel(f1);
+    var p2 = new Fixnel(f2);
 
-    //-1730 -1596 
-    var t = 0,
-        b = -173,
-        f = -159,
-        c = f -b,
-        d = 30;
-
-    var bounce = new Easing('easeOutQuart', t, b, c, d);
-    var test = document.querySelector('.test');
-    var style = test.style;
-    style.position = 'absolute';
-    style.top = 0;
-    style.left = '300px';
-    style.zIndex = 10;
-
-//    (function easing() {
-
-//        var res = bounce.getValue();
-
-//        if (res === null) {
-//            return;
-//        }
-//        style.top = res + 'px';
-//        setTimeout(easing, 1000 / 60);
-//    }());
 }(this, document));
