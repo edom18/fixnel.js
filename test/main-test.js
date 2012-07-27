22041b1028b9cc61c7ebcca4402a840394116f99
edom18 <edo.m18@gmail.com>
2012-07-27 23:15:54 +0900

    modified test.

--- test/main-test.js
+++ test/main-test.js
@@ -3,8 +3,6 @@
     'use strict';
 
     var f1 = document.querySelector('#f1');
-    var f2 = document.querySelector('#f2');
     var p = new Fixnel(f1);
-    var p2 = new Fixnel(f2);
 
 }(this, document));
