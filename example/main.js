220cf4cd451e017111ccc7e15deedb56b029a4e3
edom18 <edo.m18@gmail.com>
2012-07-31 09:00:55 +0900

    added examples.

---
+++ example/main.js
@@ -0,0 +1,10 @@
+(function (win, doc) {
+
+    'use strict';
+
+    var f1 = document.querySelector('#f1');
+    var p1 = new Fixnel(f1);
+
+    var f2 = document.querySelector('#f2');
+    var p2 = new Fixnel(f2);
+}(this, document));
