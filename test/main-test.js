52d49b815fa2dffb13a15a3497a3c8edeb4d209c
edom18 <edo.m18@gmail.com>
2012-08-04 22:07:55 +0900

    added a few test.

--- test/main-test.js
+++ test/main-test.js
@@ -4,5 +4,28 @@
 
     var f1 = document.querySelector('#f1');
     var p = new Fixnel(f1);
+    var test = document.querySelector('.faderTest');
+
+    function Dummy() {
+        this.getEl = function () {
+            return test;
+        };
+    }
+
+    var fader = new Fader(new Dummy());
+
+    var flg = false;
+    test.addEventListener('click', function () {
+
+        if (flg) {
+            flg = !flg;
+            fader.fadeIn();
+        }
+        else {
+            flg = !flg;
+            fader.fadeOut();
+        }
+    }, false);
+
 
 }(this, document));
